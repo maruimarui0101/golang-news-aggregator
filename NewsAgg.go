c87c2e4b76f9269f390ae738363ee28cb134dac5
Dolores <cd.manalansan@gmail.com>
2021-02-11 15:31:06 +0000

    Added concurrency capabilities: routines, channels, waitgroups and buffering

--- NewsAgg.go
+++ NewsAgg.go
@@ -7,7 +7,10 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 type News struct {
 	Titles    []string `xml:"url>news>title"`
@@ -33,23 +36,40 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
 }
 
+func newsRoutine(c chan News, Location string) {
+	defer wg.Done()
+	var n News
+	temp := strings.Split(Location, "\n")
+	resp, _ := http.Get(temp[1])
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &n)
+	resp.Body.Close()
+
+	c <- n
+}
+
 func newsAggHandler(w http.ResponseWriter, r *http.Request) {
 	var s SitemapIndex
-	var n News
 	news_map := make(map[string]NewsMap)
 
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-
 	xml.Unmarshal(bytes, &s)
+	resp.Body.Close()
+
+	queue := make(chan News, 30)
 
 	for _, Location := range s.Locations {
-		temp := strings.Split(Location, "\n")
-		resp, _ := http.Get(temp[1])
-		bytes, _ := ioutil.ReadAll(resp.Body)
-		xml.Unmarshal(bytes, &n)
-		for idx, _ := range n.Keywords {
-			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		wg.Add(1)
+		go newsRoutine(queue, Location)
+	}
+
+	wg.Wait()
+	close(queue)
+
+	for elem := range queue {
+		for idx, _ := range elem.Keywords {
+			news_map[elem.Titles[idx]] = NewsMap{elem.Keywords[idx], elem.Locations[idx]}
 		}
 	}
 
